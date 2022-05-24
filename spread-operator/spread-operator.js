a7a3e08685ef027c250b5e0db7eb46cddb1a98ee
cannedQualia <52677416+cannedQualia@users.noreply.github.com>
2022-05-24 16:13:32 -0500

    Added Spread operator

---
+++ spread-operator/spread-operator.js
@@ -0,0 +1,19 @@
+const fn = (a, b, c) => console.log(a, b, c)
+const arr = [1, 2, 3, 4]
+
+fn(...arr)
+
+const arr1 = [5, 6]
+
+// This is better than concat:
+const arr2 = [arr, arr1]
+const arr3 = [arr, arr1, 7, 8]
+
+const obj1 = { a: 1, b: 2 }
+const obj2 = { b: 1, c: 'This is a string', d = () => {a: `aaa`} }
+
+const obj3 = {obj1}
+obj1.a = 10
+
+const obj4 = { ...obj1, obj2}
+console.log(obj4)
